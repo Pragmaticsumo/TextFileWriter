cea3bf01829fcc3bbd360d28c25d53b88b329d63
Pragmaticsumo <71714295+Pragmaticsumo@users.noreply.github.com>
2022-07-02 16:13:06 -0400

    Code

--- TextFileWriter/Program.cs
+++ TextFileWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextFileWriter
 {
@@ -6,7 +7,23 @@ namespace TextFileWriter
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Create A File Name: ");
+            string fileName = Console.ReadLine();
+
+            if (File.Exists("TextFiles\\" + fileName + ".txt")) ;
+            {
+                File.Delete("TextFiles\\" + fileName + ".txt");
+            }
+
+            using FileStream fs = File.Create("TextFiles\\" + fileName + ".txt");
+            using var sr = new StreamWriter(fs);
+
+
+            Console.Clear();
+            Console.WriteLine("Create File Contents: ");
+            string fileContents = Console.ReadLine();
+
+            sr.WriteLine(fileContents);
         }
     }
 }
